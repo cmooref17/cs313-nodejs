c564bd5dac063590119e85e53d80c78c531e7acb
cmooref17 <cmooref17@gmail.com>
2018-06-30 16:40:04 -0600

    styling

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ function login(req, res) {
    var username = req.query.username;
    var password = req.query.password;
    
-   var db = new pg.Client(connectionString);
-   db.connect();
+   //var db = new pg.Client(connectionString);
+   //db.connect();
    
    console.log("Logging in user: " + username + " with password: " + password);
    
    var query  = 'SELECT username, password FROM "user" WHERE username = $1';
    var params = [ username ];
   
-   db.query(query, params, (err, result) => {
+   pool.query(query, params, (err, result) => {
+      if(err) {
+         console.log(err);
+         res.end(err);
+      }
       const user = result.rows[0];
       
       if(passwordHash.verify(password, user.password)) {
-         console.log("Correct password. Loggin in");
-         res.writeHead(200);
-         res.write("/project2/home.html");
-         db.end();
-         return res.end();
+         console.log("Correct password. Logging in");
+         res.end('1');
       }
       else {
          console.log("Failed to log in");
-         res.end("Failed to log in. Invalid username and/or password");
+         res.end('-1');
       }
-      db.end();
    });
 }
 
@@ -43,33 +43,45 @@ function register(req, res) {
    var hashedPassword = passwordHash.generate(password);
    console.log("Hashed password: " + hashedPassword);
    
-   var db = new pg.Client(connectionString);
-   db.connect();
-   
-   console.log("Connected to db");
+   //var db = new pg.Client(connectionString);
+   //db.connect();
    
    var query  = 'SELECT EXISTS(SELECT username FROM "user" WHERE username = $1)';
    var params = [ username ];
   
-   db.query(query, params, (err, result) => {
+   pool.query(query, params, (err, result) => {
       console.log("Testing1");
       const exists = result.rows[0]['exists'];
       if (exists) {
-         console.log("User already exists!");
-         res.end("User already exists!");
+         console.log("Username already exists!");
+         res.end('-1'); //Username already exists
       } else {
-         console.log("User doesn't exist! Good"); //Good
+         console.log("Username doesn't exist! Good"); //Good
       }
       
-      var query2 = 'INSERT INTO "user" (first_name, last_name, username, password, email) VALUES($1, $2, $3, $4, $5)';
-      var params2 = [firstName, lastName, username, hashedPassword, email];
-      db.query(query2, params2, (err, result) => {
-         if(err)
-            console.error(err);
-         else
-            console.error("No errors adding user to db");
-         res.end("Success!");
-         db.end();
+      var query1  = 'SELECT EXISTS(SELECT email FROM "user" WHERE email = $1)';
+      var params1 = [ email ];
+      
+      pool.query(query1, params1, (err, result) => {
+         console.log("Testing1");
+         const exists = result.rows[0]['exists'];
+         if (exists) {
+            console.log("Email already exists!");
+            res.end('-2'); //Email already exists
+         } else {
+            console.log("Email doesn't exist! Good"); //Good
+         }
+      
+         var query2 = 'INSERT INTO "user" (first_name, last_name, username, password, email) VALUES($1, $2, $3, $4, $5)';
+         var params2 = [firstName, lastName, username, hashedPassword, email];
+         pool.query(query2, params2, (err, result) => {
+            if(err)
+               console.error(err);
+            else
+               console.error("No errors adding user to db");
+            res.end('1');
+            //db.end();
+         });
       });
    });
 }
@@ -77,10 +89,12 @@ function register(req, res) {
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const pg = require('pg');
-const connectionString = 'postgres://mekritpfsaierc:72aaf980673b4269d1801b6a1a9cc57cb4002133545a550c330d291d943f899c@ec2-54-83-0-158.compute-1.amazonaws.com:5432/d1lvra62cii5am?ssl=true';
+//const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://mekritpfsaierc:72aaf980673b4269d1801b6a1a9cc57cb4002133545a550c330d291d943f899c@ec2-54-83-0-158.compute-1.amazonaws.com:5432/d1lvra62cii5am?ssl=true' || 'postgres://mekritpfsaierc:72aaf980673b4269d1801b6a1a9cc57cb4002133545a550c330d291d943f899c@localhost:5432/d1lvra62cii5am?ssl=true';
 
+const { Pool } = require('pg');
 const passwordHash = require('password-hash');
+var pool = new Pool({connectionString: connectionString});
 
 express()
    .use(express.json())
